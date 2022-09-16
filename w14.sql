53a4d0470e3e29b762d93f0ab08f5d74fdc4d695
Mateusz Kmon <mateusz.kmon@outlook.com>
2022-09-16 10:44:49 +0200

    add w14.sql

--- w14.sql
+++ w14.sql
@@ -1,14 +1,14 @@
-select 
+select
   to_json(
-    object_construct(      
+    object_construct(
       'country_of_residence', country_of_residence,
-      'superhero_name', superhero_name, 
-      'superpowers', 
-        case 
-        when coalesce(superpower, second_superpower, third_superpower) is null 
-          then array_construct(null) 
+      'superhero_name', superhero_name,
+      'superpowers',
+        case
+        when coalesce(superpower, second_superpower, third_superpower) is null
+          then array_construct(null)
         else array_construct_compact(superpower, second_superpower, third_superpower)
         end
     )
-  ) as superhero_json 
+  ) as superhero_json
 from week_14;
