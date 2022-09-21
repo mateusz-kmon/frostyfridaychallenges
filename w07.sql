d43be295d8f699e7b212c7fc099a47bd47611726
Mateusz Kmon <mateusz.kmon@outlook.com>
2022-09-21 10:06:29 +0200

    correct w07.sql

--- w07.sql
+++ w07.sql
@@ -1,10 +1,11 @@
 --https://frostyfriday.org/2022/07/29/week-7-intermediate/
 
 
-select tr.tag_name, tr.tag_value, min(ah.query_id), concat_ws('.',tr.object_database, tr.object_schema, tr.object_name) as table_name, qh.role_name 
-from snowflake.account_usage.tag_references tr
-join snowflake.account_usage.access_history ah 
-    on concat_ws('.',tr.object_database, tr.object_schema, tr.object_name) = ah.direct_objects_accessed[0]:objectName::string
-join snowflake.account_usage.query_history qh on ah.query_id = qh.query_id
+select tr.tag_name, tr.tag_value, min(ah.query_id), concat_ws('.',tr.object_database, tr.object_schema, tr.object_name) as table_name, qh.role_name
+from snowflake.account_usage.query_history qh
+join snowflake.account_usage.access_history ah on qh.query_id=ah.query_id
+, lateral flatten(input=>ah.direct_objects_accessed) oa
+join snowflake.account_usage.tag_references tr
+    on concat_ws('.',tr.object_database, tr.object_schema, tr.object_name) = oa.value:objectName::varchar
 where tr.tag_name='SECURITY_CLASS' and tr.tag_value='Level Super Secret A+++++++'
 group by 1,2,4,5;
