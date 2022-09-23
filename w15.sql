84b15993322a77ed5dc730c4de2e69b897518a48
Mateusz Kmon <mateusz.kmon@outlook.com>
2022-09-23 17:56:58 +0200

    add w15.sql

---
+++ w15.sql
@@ -0,0 +1,19 @@
+-- https://frostyfriday.org/2022/09/23/week-15-intermediate/
+
+create or replace function udf_bin (price number(11, 2), buckets array)
+returns number as 
+$$
+    select max(f.index)+1
+    from table(flatten(buckets)) f
+    where price>=f.value
+$$;
+
+
+select 
+  sale_date
+, price
+, udf_bin(price, [1,310000,400000,500000]) as bucket_set1
+, udf_bin(price, [210000,350000]) as bucket_set2
+, udf_bin(price, [250000,290001,320000,360000,410000,470001]) as bucket_set3
+from home_sales
+order by 1;
